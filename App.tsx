cc1702ca5c76fe83961a0e2676f3cbcad5dec32d
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-07-23 18:39:51 +0330

    005-Getting Started with the React Navigation Package
    ٥ محرم ١٤٤٥

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 
 import { StyleSheet, StatusBar } from 'react-native';
+
+import{NavigationContainer} from '@react-navigation/native';
+import{createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import CategoriesScreen from './screens/CategoriesScreen';
 
+const Stack= createNativeStackNavigator();
 export default function App() {
   return (
     <>
       <StatusBar />
-      <CategoriesScreen />
+      <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="MealsCategories" component={CategoriesScreen}/>
+            </Stack.Navigator>
+              
+      </NavigationContainer>
+      
     </>
   );
 }
