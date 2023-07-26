64718114fceb619d80bc01371ce672ede45b51f0
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-07-26 16:03:01 +0330

    011 Adding Images & Styling

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,10 +1,52 @@
-import {View,Text} from 'react-native'
-function MealItem({title}){
+import {View,Text, Pressable, Image, StyleSheet,ScrollView, Platform} from 'react-native'
+function MealItem({title,imageUrl,duration,complexity,affordability}){
     return(
-        <View>
-             <Text>{title}</Text>
-        </View>
+        <ScrollView style={styles.mealItem}>
+             <Pressable
+                android_ripple={{color:'#ccc'}}
+                style={({pressed})=> (pressed ? styles.buttonPressed : null)}
+             >
+                <View style={styles.innerContainer}>
+                    <Image source={{uri: imageUrl}} style={styles.image} />
+                    <Text style={styles.title}>{title}</Text>
+                </View>
+                <View>
+                    <Text>{duration}m</Text>
+                    <Text>{complexity}</Text>
+                    <Text>{affordability}</Text>
+                </View>
+             </Pressable>
+        </ScrollView>
     );
 }
+const styles =StyleSheet.create({
+    mealItem:{
+        margin:16,
+        borderRadius:8,
+        overflow:Platform.OS==='android'?'hidden':'visible',
+        backgroundColor:'white',
+        elevation:4,
+        shadowColor:'black',
+        shadowOpacity:0.25,
+        shadowOffset:{width:0, height:2},
+        shadowRadius:8,
+    },
+    innerContainer:{
+        borderRadius:8,
+        overflow:'hidden',
+    },
+    image:{
+            width:'100%',
+            height:200,
+    },
+    title:{
+        fontWeight:'bold',
+        textAlign:'center',
+        fontSize:18,
 
+    },
+    buttonPressed:{
+        opacity:0.5,
+    },
+})
 export default MealItem;
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -6,7 +6,15 @@ function MealsOverviewScreen({ route }){
     const {categoryId}= route?.params||{};
     const displayMeals = MEALS.filter(meal => meal.categoryIds.indexOf(categoryId) >= 0);
     function renderMealItem(itemData){
-        return <MealItem  title={itemData.item.title} />
+        const item=itemData.item;
+        const mealItemProps={
+            title:item.title,
+            imageUrl:item.imageUrl,
+            affordability:item.affordability,
+            comlexity:item.comlexity,
+            duration:item.duration,
+        };
+        return (<MealItem  {...mealItemProps} />);
     }
     return(
         <View style={styles.container}>
