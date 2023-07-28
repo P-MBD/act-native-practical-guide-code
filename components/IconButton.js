121e9ec02abbeb4481303bcf5cf927883b6f33a3
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-07-28 10:11:22 +0330

    018 Adding an Icon Button to a Header

---
+++ components/IconButton.js
@@ -0,0 +1,16 @@
+import {Pressable, StyleSheet} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+function IconButton({icon, color, onPress}){
+return(
+    <Pressable onPress={onPress} style={({pressed})=> pressed && styles.pressed }>
+         <Icon name={icon}  size={24} color={color}/> 
+    </Pressable>
+);
+}
+
+const styles = StyleSheet.create({
+    pressed:{
+        opacity:0.7
+    }
+});
+export default IconButton;
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -4,6 +4,7 @@ import List from '../components/MealDetail/List';
 import Subtitle from '../components/MealDetail/Subtitle';
 import MealDetails from '../components/MealDetails';
 import { MEALS } from '../data/dummy-data';
+import IconButton from '../components/IconButton';
 
 function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
@@ -15,7 +16,7 @@ function MealDetailScreen({ route, navigation }) {
   useLayoutEffect(()=>{
     navigation.setOptions({
         headerRight: () => {
-            return  <Button title='Tap me' onPress={headerButtonPressHandler} />
+            return  <IconButton icon='star' color="white" onPress={headerButtonPressHandler} />
         }
     });
   },[]);
